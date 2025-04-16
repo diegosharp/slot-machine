f162ec7e1c3a94564b48f8ee885e424e993dc053
diegosharp <romerolovodelad@student.douglascollege.ca>
2025-04-15 19:37:08 -0700

    Update constants_DRo786.js

--- script/constants_DRo786.js
+++ script/constants_DRo786.js
@@ -2,7 +2,7 @@ const NUMBER_OF_ITEMS_ESTIMATED = 25;
 
 const items = [
     { id: 1, name: 'Heart', odds: 1, path: 'Heart.webp', payout: 300 },
-    { id: 2, name: 'Candle Ray', odds: 3, path: 'Candle-ray.webp', payout: 100 },
+    { id: 2, name: 'Candle Ray', odds: 3, path: 'Candle-Ray.webp', payout: 100 },
     { id: 3, name: 'Piano', odds: 5, path: 'Abstract-Piano.webp', payout: 50 },
     { id: 4, name: 'Music Sheet', odds: 10, path: 'MusicSheet.webp', payout: 20 },
     { id: 5, name: 'Bookcase', odds: 15, path: 'Bookcase.webp', payout: 10 },
